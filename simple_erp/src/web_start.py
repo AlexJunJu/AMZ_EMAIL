a4d1dd7c0326a428940746ec90690ac58bb0f7ed
AlexJunJu <38601972+AlexJunJu@users.noreply.github.com>
2021-08-10 10:51:17 +0800

    Update web_start.py

--- simple_erp/src/web_start.py
+++ simple_erp/src/web_start.py
@@ -23,5 +23,49 @@ def business_report():
 	return render_template('business_report.html',BusinessReportList=BusinessReportList)
 
 
+import functools
+import collections
+
+
+def decorator(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+
+        if len(args) == 1:
+            arg = args[0]
+            if not isinstance(arg, collections.Iterable):
+                cls_name = type(arg).__name__
+                raise TypeError(f'{cls_name} is not iterable')
+
+            if not arg:
+                if 'default' in kwargs:
+                    return kwargs['default']
+                raise ValueError(f'{fn.__name__}() arg is an empty sequence')
+
+            args = arg
+
+        key_func = kwargs.get('key', None)
+
+        items = sorted(args, key=key_func)
+
+        return items[-1]
+
+    return wrapper
+
+
+@decorator
+def customized_max(*args, key=None):
+    """
+    max(iterable, *[, default=obj, key=func]) -> value
+    max(arg1, arg2, *args, *[, key=func]) -> value
+
+    With a single iterable argument, return its biggest item. The
+    default keyword-only argument specifies an object to return if
+    the provided iterable is empty.
+    With two or more arguments, return the largest argument.
+    """
+    pass
+
+
 if __name__ == '__main__':
 	app.run()
